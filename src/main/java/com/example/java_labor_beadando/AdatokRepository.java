f555c35ac98af0f16ae8af07fcac6f2a2d0e3189
DocziDavid <doczi0016@gmail.com>
2022-11-17 12:51:04 +0100

    pom.xml kiegészítése

---
+++ src/main/java/com/example/java_labor_beadando/AdatokRepository.java
@@ -0,0 +1,7 @@
+package com.example.java_labor_beadando;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdatokRepository extends CrudRepository<Meccsek, Integer> {
+
+}
