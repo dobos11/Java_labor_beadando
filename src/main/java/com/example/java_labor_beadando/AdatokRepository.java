28980945ab06b589599dd7e92ec4f9011f44bc80
DocziDavid <doczi0016@gmail.com>
2022-11-17 14:00:23 +0100

    ADatmegjelenítésen való dolgozás

--- src/main/java/com/example/java_labor_beadando/AdatokRepository.java
+++ src/main/java/com/example/java_labor_beadando/AdatokRepository.java
@@ -1,7 +1,9 @@
 package com.example.java_labor_beadando;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdatokRepository extends CrudRepository<Meccsek, Integer> {
 
 }
